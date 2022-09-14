ed5d3809ac9eedc91961d164c6d1df5eaadcb144
argentenum <38292488+argentenum@users.noreply.github.com>
2022-09-15 00:35:34 +0530

    Add files via upload

---
+++ twitterTokens.R
@@ -0,0 +1,8 @@
+create_token(
+  app = "Rjuntwitter",
+  consumer_key = "GIEBH97DmbpP0l8ene33wyj99",
+  consumer_secret = "kgimR8ZZriBAzZ69j4BE9CYmTYy392351rz0T9ldCOtzBRfC3y",
+  access_token = "56965010-tsiv5Q4gm1D3k7XTPMF4PIr7zYSqmLKmLSw47dq6n",
+  access_secret = "aQEk9LrPwouRxc4RBumMUrzRbRtgSgoZ5OOv6Cq6wgndb",
+  set_renv = TRUE
+)
